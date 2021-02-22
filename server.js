47462aefd28082b2bc22873a844b142ae4085bef
Akanopy <78884903+Akanopy@users.noreply.github.com>
2021-02-22 08:51:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ require('./util/Loader.js')(client);
 
 client.commands = new Discord.Collection(); 
 client.aliases = new Discord.Collection();  
-fs.readdir('./commands/', (err, files) => { 
+fs.readdir('./Akanokomutlar/', (err, files) => { 
   if (err) console.error(err);               
-  console.log(`${files.length} command will be loaded.`); 
+  console.log(`${files.length} Akano komutlar yüklendi.`); 
   files.forEach(f => {                       
-    let props = require(`./commands/${f}`);   
-    console.log(`${props.config.name} command installed.`); 
+    let props = require(`./Akanokomutlar/${f}`);   
+    console.log(`${props.config.name} komutlar yüklendi :O.`); 
     console.log(`successfully entered`)     
     client.commands.set(props.config.name, props); 
     props.config.aliases.forEach(alias => {          
@@ -19,5 +19,4 @@ fs.readdir('./commands/', (err, files) => {
     });
   });
 })
-
 client.login(config.token)
