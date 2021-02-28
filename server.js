72f6fad01fe4f1e3d3e05585c9eed465702d570f
Akanopy <78884903+Akanopy@users.noreply.github.com>
2021-02-28 20:13:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,71 @@
 const Discord = require("discord.js")    
 const client = new Discord.Client();       
-const config = require("./Akano-ayarlar.js")    
+const config = require("./Akanoayarlar.js")    
 const fs = require("fs");                
 require('./util/Loader.js')(client);     
 
-client.commands = new Discord.Collection(); 
-client.aliases = new Discord.Collection();  
-fs.readdir('./Akanokomutlar/', (err, files) => { 
-  if (err) console.error(err);               
-  console.log(`${files.length} Akano komutlar yüklendi.`); 
-  files.forEach(f => {                       
-    let props = require(`./Akanokomutlar/${f}`);   
-    console.log(`${props.config.name} komutlar yüklendi :O.`); 
-    console.log(`31`)     
-    client.commands.set(props.config.name, props); 
-    props.config.aliases.forEach(alias => {          
-      client.aliases.set(alias, props.config.name);  
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir("./Akanokomutlar/", (err, files) => {
+  if (err) console.error(err);
+  files.forEach(f => {
+    let props = require(`./Akanokomutlar/${f}`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
     });
   });
-})
+});
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./Akanokomutlar/${command}`)];
+      let cmd = require(`./Akanokomutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.load = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./Akanokomutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.unload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./Akanokomutlar/${command}`)];
+      let cmd = require(`./Akanokomutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
 client.login(config.token)
